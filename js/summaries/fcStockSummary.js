dafb1295122e30dec65b83c492d403cdf724c080
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-05 14:25:48 +0530

    Update fcStockSummary.js

--- js/summaries/fcStockSummary.js
+++ js/summaries/fcStockSummary.js
@@ -1,21 +1,32 @@
 /**
  * FC Wise Stock Summary
- * Input: mpPlanningRows[]
- * Output: [{ FC, "Total Stock" }]
+ *
+ * IMPORTANT:
+ * - Must be derived from fcStock
+ * - NOT from mp planning rows
+ *
+ * Input:
+ *  - fcStock[] (normalized)
+ *  - mp (string)
+ *
+ * Output:
+ *  [{ FC, "Total Stock" }]
  */
 
-export function fcStockSummary(mpRows) {
+export function fcStockSummary(fcStock, mp) {
   const map = new Map();
 
-  mpRows.forEach(r => {
-    const fc = r.fc;
-    const qty = Number(r.fcStock) || 0;
+  fcStock
+    .filter(r => r.mp === mp)
+    .forEach(r => {
+      const fc = r.warehouseId;
+      const qty = Number(r.qty) || 0;
 
-    if (!map.has(fc)) {
-      map.set(fc, 0);
-    }
-    map.set(fc, map.get(fc) + qty);
-  });
+      if (!map.has(fc)) {
+        map.set(fc, 0);
+      }
+      map.set(fc, map.get(fc) + qty);
+    });
 
   return Array.from(map.entries()).map(([fc, totalStock]) => ({
     FC: fc,
