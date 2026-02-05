1123b497178ff7fea3d6027319fd29a7a54a5dd0
SAURABH MONDAL <saurabh.jopl@gmail.com>
2026-02-05 13:57:38 +0530

    Update sources.js

---
+++ js/ingest/sources.js
@@ -0,0 +1,18 @@
+/**
+ * DATA SOURCES
+ * Read-only Google Sheets CSV endpoints
+ */
+
+export const SOURCES = {
+  sale30D:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRarC7jnt04o-cSMEJN-h3nrbNyhgd-JCoxy6B0oDwwlX09SLQjB4kMJIOkeLRXy9RId28iJjbTd8Tm/pub?gid=1268196089&single=true&output=csv",
+
+  fcStock:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRarC7jnt04o-cSMEJN-h3nrbNyhgd-JCoxy6B0oDwwlX09SLQjB4kMJIOkeLRXy9RId28iJjbTd8Tm/pub?gid=2046154602&single=true&output=csv",
+
+  uniwareStock:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRarC7jnt04o-cSMEJN-h3nrbNyhgd-JCoxy6B0oDwwlX09SLQjB4kMJIOkeLRXy9RId28iJjbTd8Tm/pub?gid=535319358&single=true&output=csv",
+
+  companyRemarks:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRarC7jnt04o-cSMEJN-h3nrbNyhgd-JCoxy6B0oDwwlX09SLQjB4kMJIOkeLRXy9RId28iJjbTd8Tm/pub?gid=998019043&single=true&output=csv"
+};
